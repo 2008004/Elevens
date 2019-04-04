cb8b224778cccce2b8191232e89f2128123600c6
2008004 <36040705+2008004@users.noreply.github.com>
2019-04-04 14:42:17 -0500

    change functions to class ver

--- src/Elevens/Deck.java
+++ src/Elevens/Deck.java
@@ -1,78 +1,125 @@
 package Elevens;
+import java.util.List;
+import java.util.ArrayList;
 
-import java.util.Arrays;
-import java.util.Collections;
-
+/**
+ * The Deck class represents a shuffled deck of cards.
+ * It provides several operations including
+ *      initialize, shuffle, deal, and check if empty.
+ */
 public class Deck {
 
-    //ace cards are = 1
-    int Ace = 1;
-    int[] values = {Ace,2,3,4,5,6,7,8,9,10,};
-    String[] suits = {"Hearts","Diamonds","Spades","Clubs"};
-    String[] special = {"King","Queen","Jack"};
-    String[] Deck;
-    int index = 0;
-    int cardsDealt = 0;
+	/**
+	 * cards contains all the cards in the deck.
+	 */
+	private List<Card> cards;
 
-    public void getDeck() {
-        for (String val:Deck){
-            System.out.println(val);
-        }
-    }
-    public String getDeckItem(int i){
-        return Deck[i];
-    }
+	/**
+	 * size is the number of not-yet-dealt cards.
+	 * Cards are dealt from the top (highest index) down.
+	 * The next card to be dealt is at size - 1.
+	 */
+	private int size = 0;
 
-    public int getCardsDealt() {
-        return cardsDealt;
-    }
 
-    public Deck(){
-        this.index = 0;
-        this.Deck = new String[52];
-        this.cardsDealt = 0;
-        //add vals for each suit
+	/**
+	 * Creates a new <code>Deck</code> instance.<BR>
+	 * It pairs each element of ranks with each element of suits,
+	 * and produces one of the corresponding card.
+     * @param ranks is an array containing all of the card ranks.
+	 * @param suits is an array containing all of the card suits.
+	 * @param values is an array containing all of the card point values.
+	 */
 
-        for (int val:values){
-            for (String suit:suits){
-                Deck[index] = suit+":"+val;
-//                System.out.println(Deck[index]);
-                index++;
+
+	public Deck(String[] ranks, String[] suits, int[] values) {
+
+
+		//face cards
+	    for (String rank:ranks){
+	        for (String suit:suits){
+	            cards.add(new Card(rank,suit,0));
+	            size++;
             }
         }
 
-        for (String special:special){
-            for(String suit:suits){
-                Deck[index] = suit+":"+special;
-//                System.out.println(Deck[index]);
-                index++;
-            }
-        }
-
-        //shuffle
-        Collections.shuffle(Arrays.asList(Deck));
-
-    }
-
-    public void dealNineCards(){
-        int rowCount = 0;
-        for (int i = cardsDealt+9; cardsDealt < i; cardsDealt++){
-//            System.out.println(cardsDealt);
-            try{
-
-                if ((rowCount+1) % 3 == 0 && rowCount != 0){
-                    System.out.println("|"+this.Deck[cardsDealt]+"|");
-                }else{
-                    System.out.print("|"+this.Deck[cardsDealt]+"|");
-                }
-
-            }catch (Exception e){
-                break;
-            }
-            rowCount++;
-        }
-    }
+        //val cards
+		for(String suit:suits){
+			for(int val:values){
+				cards.add(new Card("N/A",suit,val));
+				size++;
+			}
+		}
 
 
+	}
 
+
+	/**
+	 * Determines if this deck is empty (no undealt cards).
+	 * @return true if this deck is empty, false otherwise.
+	 */
+	public boolean isEmpty() {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+	}
+
+	/**
+	 * Accesses the number of undealt cards in this deck.
+	 * @return the number of undealt cards in this deck.
+	 */
+	public int Getsize() {
+		return size;
+	}
+
+	/**
+	 * Randomly permute the given collection of cards
+	 * and reset the size to represent the entire deck.
+	 */
+	public void shuffle() {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 4 *** */
+	}
+
+	/**
+	 * Deals a card from this deck.
+	 * @return the card just dealt, or null if all the cards have been
+	 *         previously dealt.
+	 */
+	public Card deal() {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+	}
+
+	/**
+	 * Generates and returns a string representation of this deck.
+	 * @return a string representation of this deck.
+	 */
+	@Override
+	public String toString() {
+		String rtn = "size = " + size + "\nUndealt cards: \n";
+
+		for (int k = size - 1; k >= 0; k--) {
+			rtn = rtn + cards.get(k);
+			if (k != 0) {
+				rtn = rtn + ", ";
+			}
+			if ((size - k) % 2 == 0) {
+				// Insert carriage returns so entire deck is visible on console.
+				rtn = rtn + "\n";
+			}
+		}
+
+		rtn = rtn + "\nDealt cards: \n";
+		for (int k = cards.size() - 1; k >= size; k--) {
+			rtn = rtn + cards.get(k);
+			if (k != size) {
+				rtn = rtn + ", ";
+			}
+			if ((k - cards.size()) % 2 == 0) {
+				// Insert carriage returns so entire deck is visible on console.
+				rtn = rtn + "\n";
+			}
+		}
+
+		rtn = rtn + "\n";
+		return rtn;
+	}
 }
---
+++ src/Elevens/DeckTester.java
@@ -0,0 +1,17 @@
+package Elevens;
+/**
+ * This is a class that tests the Deck class.
+ */
+public class DeckTester {
+
+	/**
+	 * The main method in this class checks the Deck operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		String[] ranks = {"king","queen","Jack","Ace"};
+		String[] suits =  {"hearts","diamonds","clubs","spades"};
+		int[] values = {1,2,3,4,5,6,7,8,9};
+		Deck gameDeck
+	}
+}
