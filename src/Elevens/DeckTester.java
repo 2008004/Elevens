af2c06d6aaa85545e85a404d12d562108d9b8bae
2008004 <36040705+2008004@users.noreply.github.com>
2019-04-17 14:27:10 -0500

    diplayed cards

--- src/Elevens/DeckTester.java
+++ src/Elevens/DeckTester.java
@@ -11,31 +11,75 @@ import java.util.List;
  */
 public class DeckTester {
 
-	/**
-	 * The main method in this class checks the Deck operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		boolean isEmpty = false;
-		boolean boardEmpty = false;
-		String[] ranks = {"K","Q","J","A"};
-		String[] suits =  {"H","D","C","S"};
-		int[] values = {1,2,3,4,5,6,7,8,9};
-		Deck gameDeck = new Deck(ranks,suits,values);
+    /**
+     * The main method in this class checks the Deck operations for consistency.
+     *
+     * @param args is not used.
+     */
+    public static void main(String[] args) {
+        boolean isEmpty = false;
+        boolean boardEmpty = false;
+        String[] ranks = {"K", "Q", "J", "A"};
+        String[] suits = {"H", "D", "C", "S"};
+        int[] values = {2, 3, 4, 5, 6, 7, 8, 9};
+        Deck gameDeck = new Deck(ranks, suits, values);
 
-		Card[][] gameBoard = new Card[3][3];
-		for (Card[] row:gameBoard){
-			for (Card column:row){
-				column = null;
-			}
-		}
-
-		gameDeck.realShuffle();
-
-		JFrame frame = new JFrame();
+        Card[][] gameBoard = new Card[3][3];
+        for (Card[] row : gameBoard) {
+            for (Card column : row) {
+                column = null;
+            }
+        }
 
 
+        JFrame frame = new JFrame("Elevens");
+        GridLayout gridLayout = new GridLayout(3, 4, 50, 50);
+        frame.setLayout(gridLayout);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-		}
-	}
+        int rowCount = 0;
+        int colCount = 0;
+
+        gameDeck.realShuffle();
+        while (!isEmpty) {
+            for (Card[] row : gameBoard) {
+                for (Card column : row) {
+                    if (column == null) {
+                        gameBoard[rowCount][colCount] = gameDeck.deal();
+                    }
+                    colCount++;
+//                    System.out.println(column);
+                }
+                rowCount++;
+                colCount = 0;
+            }
+
+
+            for (Card[] row : gameBoard) {
+                for (Card column : row) {
+                    System.out.println(column);
+
+                    if (column.pointValue() != 0){
+                        ImageIcon cardImage = new ImageIcon("images/"+column.pointValue()+column.suit()+".jpg");
+                        JButton a = new JButton("",cardImage);
+                        frame.add(a);
+                    }else{
+                        ImageIcon cardImage = new ImageIcon("images/"+column.rank()+column.suit()+".jpg");
+                        JButton a = new JButton("",cardImage);
+                        frame.add(a);
+                    }
+
+
+                }
+            }
+
+            frame.pack();
+            frame.setVisible(true);
+
+
+            isEmpty = gameDeck.isEmpty();
+            isEmpty = true;
+        }
+    }
+}
 
